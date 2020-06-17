b8467ccf2a2337f8ffabb197c6515b763c4821b4
nathangrigne <nathan.grigne@hetic.net>
2020-06-17 14:25:00 +0200

    :wrench: add controllers

---
+++ controllers/decay.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Decay';
+$scripts = ['public/assets/decay.js'];
+$styles = ['public/assets/decay.css'];
+
+include '../views/pages/decay.php';
---
+++ views/pages/decay.php
@@ -0,0 +1,3 @@
+<?php include '../views/partials/header.php' ?>
+    <h2>OUI</h2>
+<?php include '../views/partials/footer.php' ?>
